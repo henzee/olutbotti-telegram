b3531c973d2bc0fd05eacbadf67cbecc9fb40a55
Heikki Säily <heikkisaily@Heikki-Saily.local>
2017-02-16 14:00:28 +0200

    save

---
+++ Procfile
@@ -0,0 +1,1 @@
+web node kalja.js
---
+++ kalja.js
@@ -0,0 +1,15 @@
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const packageInfo = require('./package.json');
+
+const token = '357991658:AAFKTSI_v1xxOWKGVBIXynRrI-xSaFsyJrY';
+const telegram = new TelegramBot(token, { polling: true });
+
+telegram.on('Kaljaa', (message) => {
+  console.log(message);
+  telegram.sendMessage(message.chat.id, 'Maistuisko?');
+});
+
+const app = express();
+app.get('/', (req, res) => res.json({ version: packageInfo.version }));
+app.listen(process.env.PORT || 3000, () => console.log('Maistuisko?'));
