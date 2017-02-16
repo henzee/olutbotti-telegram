864bf968af4db0171f9ee006a9f0a8400642531a
Heikki Säily <heikkisaily@Heikki-Saily.local>
2017-02-16 14:26:11 +0200

    Token

--- kalja.js
+++ kalja.js
@@ -1,15 +1,17 @@
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const packageInfo = require('./package.json');
-const token = ''
+var token;
+var brewtoken;
 
 if (process.env.NODE && process.env.NODE.indexOf("heroku")) {
     console.log("I'm in Heroku!");
     token = process.env.TELEGRAM_TOKEN;
-    const brewtoken = process.env.BREWERYDB_TOKEN;
+    brewtoken = process.env.BREWERYDB_TOKEN;
 }
 else {
     token = '357991658:AAFKTSI_v1xxOWKGVBIXynRrI-xSaFsyJrY';
+    brewtoken = 'bd262cd1935c4c593bffe90e4305c20f';
 }
 const telegram = new TelegramBot(token, { polling: true });
 
